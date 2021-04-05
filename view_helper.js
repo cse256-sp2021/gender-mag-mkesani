08bc2eaf69b0251cdb0b7b2d7820eb6c8ad09d33
mkesani <54540554+mkesani@users.noreply.github.com>
2021-04-05 09:52:32 -0500

    Update view_helper.js

--- view_helper.js
+++ view_helper.js
@@ -504,12 +504,11 @@ function define_new_user_select_field(id_prefix, select_button_text, on_user_cha
 // Get a (very simple) text representation of a permissions explanation
 function get_explanation_text(explanation) {
     return `
-    Action allowed?: ${explanation.is_allowed}; 
+    Action allowed?: ${explanation.is_allowed};
     Because of
     permission set for file: ${explanation.file_responsible?get_full_path(explanation.file_responsible):'N/A'}
     and for user: ${ explanation.ace_responsible ? get_user_name(explanation.ace_responsible.who) : 'N/A' }
-    ${ explanation.text_explanation ? `(${explanation.text_explanation})`  : '' }
-    `
+    ${ explanation.text_explanation ? `(${explanation.text_explanation})`  : '' }   `
 }
 
 //---- some universal HTML set-up so you don't have to do it in each wrapper.html ----
